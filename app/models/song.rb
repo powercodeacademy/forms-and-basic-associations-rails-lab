2cf8152991c2257c98f239b35e398cb996158daf
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-21 12:10:39 -0400

    Add notes to a song

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,5 +10,16 @@ class Song < ApplicationRecord
   def artist_name 
     self.artist ? self.artist.name : nil 
   end
+
+  def note_contents=(contents)
+    contents.each do |content| 
+      next if content.blank?
+      Note.find_or_create_by(content: content, song: self)
+    end
+  end
+
+  def note_contents 
+    self.notes.map(&:content)
+  end
   
 end
