791880c4acc61e8837b29dabb22f0709603c9811
Mike <mclancy96@gmail.com>
2025-08-21 09:59:43 -0400

    Add note_contents

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
+require 'byebug'
 class Song < ApplicationRecord
-  belongs_to :artist
-  belongs_to :genre
+  belongs_to :artist, optional: true
+  belongs_to :genre, optional: true
   has_many :notes
 
   def artist_name
@@ -10,5 +11,16 @@ class Song < ApplicationRecord
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
     save
+  end
+
+  def note_contents
+    notes.map(&:content)
+  end
+
+  def note_contents=(notes)
+    notes.each do |note|
+      found_note = Note.find_or_create_by(content: note)
+      self.notes << found_note
+    end
   end
 end
