140d9426a971937ee6445ba9dc776be2a9b884b1
Nihar Patel <niharpatel52@gmail.com>
2025-08-21 11:34:54 -0400

    Create Fields for 3 notes, and add setter/getter for notes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist, optional: true
   belongs_to :genre, optional: true
+  has_many :notes
 
   def artist_name
     artist&.name
@@ -9,4 +10,17 @@ class Song < ApplicationRecord
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
   end
+
+  def note_contents
+    notes.map(&:content)
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      next if content.blank?
+
+      Note.find_or_create_by(content: content, song: self)
+    end
+  end
+
 end
