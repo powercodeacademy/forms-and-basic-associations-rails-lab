c5310f10d09f6217ef15baa7205c1a94088a2db5
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-21 07:08:12 -0400

    Create genre in test

--- spec/features/song_form_spec.rb
+++ spec/features/song_form_spec.rb
@@ -19,13 +19,14 @@ describe "the song form", :type => :feature do
   end
 
   it 'creates a song with a genre' do
+    genre = Genre.create(name: "Alternative")
     visit '/songs/new'
     fill_in :song_artist_name, with: 'Tori Amos'
     fill_in :song_title, with: 'Little Earthquakes'
     select 'Alternative', from: :song_genre_id
     find('input[name="commit"]').click
     expect(Song.last.genre.name).to eq 'Alternative'
-    expect(Genre.find_by(name: 'Alternative').songs.pluck(:title)).to include 'Little Earthquakes'
+    expect(genre.songs.pluck(:title)).to include 'Little Earthquakes'
   end
 
   it 'creates a song with notes' do
