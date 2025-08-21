1e2682ef12753d1786ce1aee943f7d9d1278386c
Mike <mclancy96@gmail.com>
2025-08-21 09:48:35 -0400

    Add artist_name

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,13 @@ class Song < ApplicationRecord
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+  def artist_name
+    artist.name
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+    save
+  end
 end
