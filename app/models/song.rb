922ad2aa2226f0170b1cd058d1c329cd00beb29e
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-21 08:55:31 -0400

    Create new songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,22 @@
 class Song < ApplicationRecord
-  belongs_to :artist 
-  belongs_to :genre 
+  belongs_to :artist, optional: true
+  belongs_to :genre, optional: true
   has_many :notes 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name 
+    self.artist ? self.artist.name : nil 
+  end
+
+  # def genre_name=(name)
+  #   self.genre = Genre.find_or_create_by(name: name)
+  # end
+
+  # def genre_name 
+  #   self.genre ? self.genre.name : nil 
+  # end
+  
 end
