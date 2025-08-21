92e01374216505ae2c53ad504f56a7229d57b6f4
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-21 08:57:47 -0400

    Delete commented code

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,13 +10,5 @@ class Song < ApplicationRecord
   def artist_name 
     self.artist ? self.artist.name : nil 
   end
-
-  # def genre_name=(name)
-  #   self.genre = Genre.find_or_create_by(name: name)
-  # end
-
-  # def genre_name 
-  #   self.genre ? self.genre.name : nil 
-  # end
   
 end
