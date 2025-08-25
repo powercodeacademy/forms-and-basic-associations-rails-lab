bb61c73edb2c80a059b52321768341488889cf40
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-25 11:37:43 -0400

    Create notes

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist, optional: true
   belongs_to :genre, optional: true
+  has_many :notes
 
   def artist_name
     artist&.name
@@ -9,5 +10,16 @@ class Song < ApplicationRecord
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def note_contents
+    self.notes.map(&:content)
+    # self.notes.map { |note| note.content }
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      self.notes << Note.create(content: content)
+    end
   end
 end
